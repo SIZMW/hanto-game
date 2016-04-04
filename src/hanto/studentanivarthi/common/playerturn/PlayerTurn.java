d8eba244bd2fe7397029f8453f91322ad88bbe1b
Aditya <adityanivarthi19@gmail.com>
2016-04-03 21:55:44 -0400

    Player turn comments and formatting.

--- src/hanto/studentanivarthi/common/playerturn/PlayerTurn.java
+++ src/hanto/studentanivarthi/common/playerturn/PlayerTurn.java
@@ -1,3 +1,7 @@
+/**
+ * This class was created for the Hanto game implementation for CS 4233.
+ */
+
 package hanto.studentanivarthi.common.playerturn;
 
 import java.util.Optional;
@@ -6,17 +10,56 @@ import hanto.common.HantoCoordinate;
 import hanto.common.HantoPlayerColor;
 import hanto.studentanivarthi.common.piecemanager.HantoPlayerPieceManager;
 
+/**
+ * The PlayerTurn interface defines the properties of a player turn that need to
+ * be stored during the Hanto game.
+ *
+ * @author Aditya Nivarthi
+ */
 public interface PlayerTurn {
 
+    /**
+     * Returns the color of the player associated with this turn.
+     *
+     * @return a {@link HantoPlayerColor}
+     */
     public HantoPlayerColor getColor();
 
-    public HantoPlayerPieceManager getPlayerPieceManager();
-
-    public int getTurnCount();
-
-    public void updateTurnCount(int update);
-
+    /**
+     * Returns this player's butterfly coordinate as an {@link Optional}, to
+     * handle if the butterfly has not been placed.
+     *
+     * @return an {@link Optional}&lt;{@link HantoCoordinate}&gt;
+     */
     public Optional<HantoCoordinate> getPlayerButterflyCoordinate();
 
+    /**
+     * Returns the player piece manager for this player.
+     *
+     * @return a {@link HantoPlayerPieceManager}
+     */
+    public HantoPlayerPieceManager getPlayerPieceManager();
+
+    /**
+     * Returns the number of turns this player has played.
+     *
+     * @return an integer
+     */
+    public int getTurnCount();
+
+    /**
+     * Sets the player's butterfly location to the specified coordinate.
+     *
+     * @param coord
+     *            The {@link HantoCoordinate} of the butterfly.
+     */
     public void setPlayerButterflyCoordinate(HantoCoordinate coord);
+
+    /**
+     * Updates the turn count by the specified amount.
+     * 
+     * @param update
+     *            The integer value to update by.
+     */
+    public void updateTurnCount(int update);
 }
--- src/hanto/studentanivarthi/common/playerturn/PlayerTurnImpl.java
+++ src/hanto/studentanivarthi/common/playerturn/PlayerTurnImpl.java
@@ -1,3 +1,7 @@
+/**
+ * This class was created for the Hanto game implementation for CS 4233.
+ */
+
 package hanto.studentanivarthi.common.playerturn;
 
 import java.util.Optional;
@@ -6,38 +10,67 @@ import hanto.common.HantoCoordinate;
 import hanto.common.HantoPlayerColor;
 import hanto.studentanivarthi.common.piecemanager.HantoPlayerPieceManager;
 
+/**
+ * The implementation for the PlayerTurn interface.
+ *
+ * @author Aditya Nivarthi
+ */
 public class PlayerTurnImpl implements PlayerTurn {
     private HantoPlayerColor color;
     private HantoPlayerPieceManager pieceManager;
     private int turnCount = 0;
     private Optional<HantoCoordinate> butterflyCoordinate;
 
+    /**
+     * Creates a PlayerTurnImpl instance with the specified color and piece
+     * manager.
+     *
+     * @param color
+     *            The {@link HantoPlayerColor} of the player.
+     * @param pieceManager
+     *            The {@link HantoPlayerPieceManager} for this player.
+     */
     public PlayerTurnImpl(HantoPlayerColor color, HantoPlayerPieceManager pieceManager) {
         this.color = color;
         this.pieceManager = pieceManager;
         butterflyCoordinate = Optional.empty();
     }
 
+    /**
+     * @see {@link hanto.studentanivarthi.common.playerturn.PlayerTurn#getColor()}
+     */
     @Override
-    public HantoPlayerPieceManager getPlayerPieceManager() {
-        return pieceManager;
+    public HantoPlayerColor getColor() {
+        return color;
     }
 
-    @Override
-    public int getTurnCount() {
-        return turnCount;
-    }
-
-    @Override
-    public void updateTurnCount(int update) {
-        turnCount += update;
-    }
-
+    /**
+     * @see {@link hanto.studentanivarthi.common.playerturn.PlayerTurn#getPlayerButterflyCoordinate()}
+     */
     @Override
     public Optional<HantoCoordinate> getPlayerButterflyCoordinate() {
         return butterflyCoordinate;
     }
 
+    /**
+     * @see {@link hanto.studentanivarthi.common.playerturn.PlayerTurn#getPlayerPieceManager()}
+     */
+    @Override
+    public HantoPlayerPieceManager getPlayerPieceManager() {
+        return pieceManager;
+    }
+
+    /**
+     * @see {@link hanto.studentanivarthi.common.playerturn.PlayerTurn#getTurnCount()}
+     */
+    @Override
+    public int getTurnCount() {
+        return turnCount;
+    }
+
+    /**
+     * @see {@link hanto.studentanivarthi.common.playerturn.PlayerTurn#setPlayerButterflyCoordinate(hanto.common.HantoCoordinate)}
+     */
     @Override
     public void setPlayerButterflyCoordinate(HantoCoordinate coord) {
         if (coord != null) {
@@ -45,8 +78,11 @@ public class PlayerTurnImpl implements PlayerTurn {
         }
     }
 
+    /**
+     * @see {@link hanto.studentanivarthi.common.playerturn.PlayerTurn#updateTurnCount(int)}
+     */
     @Override
-    public HantoPlayerColor getColor() {
-        return color;
+    public void updateTurnCount(int update) {
+        turnCount += update;
     }
 }
