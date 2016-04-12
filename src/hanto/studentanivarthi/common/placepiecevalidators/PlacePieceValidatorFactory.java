460c4a9389699fb725f95ca1562f6c2059b13b51
Aditya <adityanivarthi19@gmail.com>
2016-04-12 12:02:03 -0400

    Updated PlacePieceValidatorFactory to account for the game type. Made BetaHantoGame extend AbstractHantoGame.

--- src/hanto/studentanivarthi/common/placepiecevalidators/PlacePieceValidatorFactory.java
+++ src/hanto/studentanivarthi/common/placepiecevalidators/PlacePieceValidatorFactory.java
@@ -4,6 +4,7 @@
 
 package hanto.studentanivarthi.common.placepiecevalidators;
 
+import hanto.common.HantoGameID;
 import hanto.studentanivarthi.common.movevalidators.MoveValidator;
 
 /**
@@ -36,13 +37,39 @@ public class PlacePieceValidatorFactory {
      * Returns the {@link MoveValidator} associated with the specified point in
      * time during the Hanto game.
      *
+     * @param gameID
+     *            The {@link HantoGameID} of the game.
      * @param isFirstMove
      *            The state of whether the game has just started.
      * @param totalTurnCount
      *            The number of turns that have passed in the game.
      * @return a {@link PlacePieceValidator}
      */
-    public PlacePieceValidator getPlacePieceValidator(boolean isFirstMove, int totalTurnCount) {
+    public PlacePieceValidator getPlacePieceValidator(HantoGameID gameID, boolean isFirstMove,
+            int totalTurnCount) {
+        switch (gameID) {
+            case BETA_HANTO:
+                return getBetaHantoPlacePieceValidator(isFirstMove, totalTurnCount);
+            case GAMMA_HANTO:
+                return getGammaHantoPlacePieceValidator(isFirstMove, totalTurnCount);
+            case DELTA_HANTO:
+                return null; // TODO Fix this and alpha
+            default:
+                return null;
+        }
+    }
+
+    protected PlacePieceValidator getBetaHantoPlacePieceValidator(boolean isFirstMove,
+            int totalTurnCount) {
+        if (isFirstMove) {
+            return new FirstTurnPlacePieceValidator();
+        } else {
+            return new SecondTurnPlacePieceValidator();
+        }
+    }
+
+    protected PlacePieceValidator getGammaHantoPlacePieceValidator(boolean isFirstMove,
+            int totalTurnCount) {
         if (isFirstMove) {
             return new FirstTurnPlacePieceValidator();
         } else if (totalTurnCount < FIRST_TURN_CYCLE) {
